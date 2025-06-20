69052d9deb9da1db6255dd28006db785709fbe2e
paulathejennifer <paulathejennifer@gmail.com>
2025-06-20 16:59:07 +0300

    Created Cooperative Official application for Imbuto Hub project

--- imbuto_hub/cooperativeOfficials/models.py
+++ imbuto_hub/cooperativeOfficials/models.py
@@ -6,7 +6,7 @@ class CooperativeOfficial(models.Model):
     official_id = models.AutoField(primary_key=True)
     full_name = models.CharField(max_length=100)
     username = models.CharField(max_length=100, unique=True)
-    password = models.CharField(max_length=128)  
+    password = models.CharField(max_length=128)  # Same: hash this
     email = models.EmailField(unique=True)
     role = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
