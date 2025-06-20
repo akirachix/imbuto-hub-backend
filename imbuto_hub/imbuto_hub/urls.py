6f20f198e7641404f3294850803f182be752ad1a
Angela Katola Wanjala <angelakatolawanjala@gmail.com>
2025-06-20 14:42:25 +0300

    created a project

--- imbuto_hub/imbuto_hub/urls.py
+++ imbuto_hub/imbuto_hub/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for imbuto project.
+URL configuration for imbuto_hub project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
