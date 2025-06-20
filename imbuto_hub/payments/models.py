e5060509afad9a6aa4a48a482712dcfa8e696203
Angela Katola Wanjala <angelakatolawanjala@gmail.com>
2025-06-20 16:37:41 +0300

    added my model

---
+++ imbuto_hub/payments/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Payment(models.Model):
+    payment_id = models.AutoField(primary_key=True)
+    payment_status = models.CharField(max_length=20, choices=[('Pending', 'Pending'), ('Paid', 'Paid')])
+    price_per_ltr = models.DecimalField(max_digits=10, decimal_places=2)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    farmer_id = models.ForeignKey(Farmer, on_delete=models.CASCADE, related_name='payments')
+    payment_date = models.DateField()
+    records_id = models.ManyToManyField(MilkRecords, related_name='payments')
+
+    def __str__(self):
+        return f"Payment {self.payment_id} - {self.farmer.first_name}"
+
+
