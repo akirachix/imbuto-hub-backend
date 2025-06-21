ba5f60f2fd76e84bd041f9dbae3bfd45f29114ff
Angela Katola Wanjala <angelakatolawanjala@gmail.com>
2025-06-21 11:53:54 +0300

    made changes to settings.py

--- imbutohub/imbutohub/settings.py
+++ imbutohub/imbutohub/settings.py
@@ -74,13 +74,8 @@ WSGI_APPLICATION = 'imbutohub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'imbutoHub',
-        'USER':'psotgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT':'5432',
-        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
