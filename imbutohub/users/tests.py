57812372e24afcd91a7ed627a2a02b048ee115ab
paulathejennifer <paulathejennifer@gmail.com>
2025-07-01 15:31:38 +0300

    Users endpoints with running tests

--- imbutohub/users/tests.py
+++ imbutohub/users/tests.py
@@ -62,3 +62,5 @@ class UserEndpointTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]['user_type'], 'farmer')
+
+        
