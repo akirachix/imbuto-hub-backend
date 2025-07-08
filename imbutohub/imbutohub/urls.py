d321861c4c183d5b40d4143372b5bfd927c30d22
paulathejennifer <paulathejennifer@gmail.com>
2025-07-08 20:10:39 +0300

    Removed duplicated imports in imbutohub/urls

--- imbutohub/imbutohub/urls.py
+++ imbutohub/imbutohub/urls.py
@@ -24,24 +24,18 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('api.urls')),
 ]
-from django.contrib import admin
-from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
 
-from django.contrib import admin
-from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
 ]
 
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 if settings.DEBUG:
