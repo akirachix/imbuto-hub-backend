f101f7df5048196862b0bfb02288d3f5ed8aafe9
Angela Katola Wanjala <angelakatolawanjala@gmail.com>
2025-07-11 15:54:41 +0300

    worked on intergrations

--- imbutohub/Payments/models.py
+++ imbutohub/Payments/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+
 class Payment(models.Model):
     payment_id = models.AutoField(primary_key=True)
     payment_status = models.CharField(max_length=20, choices=[('Pending', 'Pending'), ('Paid', 'Paid')])
     price_per_ltr = models.DecimalField(max_digits=10, decimal_places=2)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    # farmer_id = models.ForeignKey('FarmerDetails.Farmer', on_delete=models.CASCADE, related_name='payments')
+    # member_id = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='payments')
     payment_date = models.DateField()
     # records_id = models.ManyToManyField('milkRecords.MilkRecord', related_name='payments')
 
