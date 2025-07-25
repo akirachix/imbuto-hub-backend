f574eeb77a6f8295f07b272bd32c7e54468a4c4d
Angela Katola Wanjala <angelakatolawanjala@gmail.com>
2025-07-25 18:42:15 +0300

    made changes on the milk records

--- milkRecords/models.py
+++ milkRecords/models.py
@@ -6,10 +6,12 @@ from users.models import User
 class MilkRecord(models.Model):
     record_id = models.AutoField(primary_key=True)
     member_id= models.ForeignKey(User, on_delete=models.CASCADE, related_name='milk_records')
+    quantity= models.DecimalField(max_digits=10, decimal_places=2)
     amount_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField()
     def __str__(self):
         return f"MilkRecord {self.record_id} - {self.member_id.first_name}"
+
 
 
 
